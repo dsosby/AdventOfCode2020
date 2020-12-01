e7b2d0cd9c9da78a7120a2de07a78d3d99429b76
David Sosby <david@sosby.org>
2020-11-30 22:06:22 -0800

    First crack... ugly

--- Solutions/Day1.fs
+++ Solutions/Day1.fs
@@ -1,5 +1,25 @@
 ﻿namespace AdventOfCode2020
 
+open System
+
 module Solutions =
-    let Day1 name =
-        "Unsolved"
+
+    let solve (input : string) =
+        let nums = input.Split "\n" |> Array.map int
+        let targetCondition = (fun (lhv, rhv) -> lhv + rhv = 2020)
+        let maxidx = nums.Length - 1
+        let sums = seq {
+            for i in 0..maxidx do
+                for j in i..maxidx do
+                    (nums.[i], nums.[j])
+        }
+        let pair = Seq.tryFind targetCondition sums
+        Option.map (fun (lhv, rhv) -> lhv * rhv) pair
+
+    let Day1 input =
+        match input with
+        | (null|"") -> "Bad travel expenses"
+        | input ->
+            match (solve input) with
+            | None -> "Nothing found"
+            | Some solution -> solution.ToString()

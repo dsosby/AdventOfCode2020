bfd0f5964957176786835d7bf304f1e6d5d46085
David Sosby <david@sosby.org>
2020-12-01 17:35:11 -0800

    Hacky part 2
    Needs some cleanup

--- Solutions/Day1.fs
+++ Solutions/Day1.fs
@@ -4,12 +4,21 @@ open System
 
 module Solutions =
 
-    let private commutativePairs (nums : int [])=
+    let private commutativePairs (nums : int []) =
         let maxidx = nums.Length - 1
         seq {
             for i in 0..maxidx do
                 for j in i..maxidx do
                     (nums.[i], nums.[j])
+        }
+
+    let private commutativeTriplets (nums : int []) =
+        let maxidx = nums.Length - 1
+        seq {
+            for i in 0..maxidx do
+                for j in i..maxidx do
+                    for k in j..maxidx do
+                        (nums.[i], nums.[j], nums.[k])
         }
 
     let private solve (input : string) =
@@ -19,10 +28,25 @@ module Solutions =
         |> Seq.tryFind (fun (lhv, rhv) -> lhv + rhv = 2020)
         |> Option.map (fun (lhv, rhv) -> lhv * rhv)
 
+    let private solvePart2 (input : string) =
+        input.Split [|'\n'|]
+        |> Array.map int
+        |> commutativeTriplets
+        |> Seq.tryFind (fun (lhv, rhv, rhvp) -> lhv + rhv + rhvp = 2020)
+        |> Option.map (fun (lhv, rhv, rhvp) -> lhv * rhv * rhvp)
+
     let Day1 input =
         match input with
         | (null|"") -> "Bad travel expenses"
         | input ->
             match (solve input) with
+            | None -> "Nothing found"
+            | Some solution -> solution.ToString()
+
+    let Day1Part2 input = 
+        match input with
+        | (null|"") -> "Bad travel expenses"
+        | input ->
+            match (solvePart2 input) with
             | None -> "Nothing found"
             | Some solution -> solution.ToString()
--- Web/Functions.cs
+++ Web/Functions.cs
@@ -11,12 +11,21 @@ namespace AdventOfCode2020.Web
     public static class Functions
     {
         [FunctionName("Day1")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> Day1(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             return new OkObjectResult(Solutions.Day1(requestBody));
+        }
+
+        [FunctionName("Day1Part2")]
+        public static async Task<IActionResult> Day1Part2(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            return new OkObjectResult(Solutions.Day1Part2(requestBody));
         }
     }
 }

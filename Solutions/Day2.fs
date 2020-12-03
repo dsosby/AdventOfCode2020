3ecb13cf72ba7fbdae2d2e3a0c0bc1c0ba155ff5
David Sosby <david@sosby.org>
2020-12-03 10:30:17 -0800

    Refactor/clean Day2

--- Solutions/Day2.fs
+++ Solutions/Day2.fs
@@ -6,54 +6,47 @@ let samples = @"1-3 a: abcde
 2-9 c: ccccccccc"
 // Correct ones are lines 1 and 3
 
-let countLetters (letter : char) (str : string) =
-    str
-    |> Seq.sumBy (fun c ->
-        match c with
-        | c when c = letter -> 1
+let countIf pred seq =
+    let oneIfTrue = function
+        | c when (pred c) -> 1
         | _ -> 0
-    )
+    seq
+    |> Seq.sumBy oneIfTrue
+
 
 let isValidPassword (value : string) =
     let parts = value.Split [| ':' |]
-    match parts with
-    | [| rule;password |] ->
-        let ruleparts = rule.Split [| '-'; ' ' |]
-        let mincount = int ruleparts.[0]
-        let maxcount = int ruleparts.[1]
-        let letter = ruleparts.[2].[0]
-        let lettercount = countLetters letter password
-        match lettercount with
-        | c when c >= mincount && c <= maxcount -> true
-        | _ -> false
-    | _ -> false
+    let rule = parts.[0]
+    let password = parts.[1]
+    let ruleparts = rule.Split [| '-'; ' ' |]
+    let minCount = int ruleparts.[0]
+    let maxCount = int ruleparts.[1]
+    let letter = ruleparts.[2].[0]
+
+    let letterCount = password |> countIf ((=) letter)
+    letterCount >= minCount && letterCount <= maxCount
 
 let solvePart1 (values : string) =
     // this would be fun with fparsec or something
     values.Split [| '\n' |]
-    |> Seq.map isValidPassword
-    |> Seq.filter (fun x -> true = x) // Is there a better way to express a truth predicate?
-    |> Seq.length
+    |> countIf isValidPassword
     |> Some
 
 let isValidPasswordToo (value : string) =
     let parts = value.Split [| ':' |]
-    match parts with
-    | [| rule;password |] ->
-        let ruleparts = rule.Split [| '-'; ' ' |]
-        let indexone = int ruleparts.[0]
-        let indextwo = int ruleparts.[1]
-        let letter = ruleparts.[2].[0]
-        let letterone = password.[indexone] // I didn't strip the leading space, so get 1-based index for free
-        let lettertwo = password.[indextwo]
-        let onlyOneValid = (letterone = letter && lettertwo <> letter) || (letterone <> letter && lettertwo = letter) // xor?
-        onlyOneValid
-    | _ -> false
+    let rule = parts.[0]
+    let password = parts.[1]
+    let ruleparts = rule.Split [| '-'; ' ' |]
+    let indexone = int ruleparts.[0]
+    let indextwo = int ruleparts.[1]
+    let letter = ruleparts.[2].[0]
+    let letterone = password.[indexone] // I didn't strip the leading space, so get 1-based index for free
+    let lettertwo = password.[indextwo]
+
+    (letterone = letter && lettertwo <> letter) || (letterone <> letter && lettertwo = letter) // xor?
 
 let solvePart2 (values : string) =
     values.Split [| '\n' |]
-    |> Seq.map isValidPasswordToo
-    |> Seq.filter (fun x -> true = x) // Is there a better way to express a truth predicate?
-    |> Seq.length
+    |> countIf isValidPasswordToo
     |> Some
 

6f8f95521786deab434e7f0900c4351d2f8d9716
David Sosby <dsosby@gmail.com>
2020-12-08 12:16:54 -0800

    Lunch-time coding

--- Solutions/Day5.fs
+++ Solutions/Day5.fs
@@ -29,10 +29,31 @@ let findSeat (input: string) =
     let cabinWidth = 8
     row, col, row * 8 + col
 
-[<Solution(part = 1)>]
-let solve (input: string) =
+let parseSeats (input: string) =
     input.Trim().Split("\n")
     |> Seq.map findSeat
+
+[<Solution(part = 1)>]
+let findMaxSeatId (input: string) =
+    input
+    |> parseSeats
     |> Seq.map (fun (row, col, id) -> id)
     |> Seq.max
 
+[<Solution(part = 2)>]
+let findMissingValue (input: string) =
+    let seats =
+        input
+        |> parseSeats
+        |> Seq.sort
+        |> Seq.toList
+
+    // Find the seat where id - offset != idx
+    // No Seq.findi, so temp do it with mapi and find
+    let (_, _, offset) = seats.[0]
+    let (_, outOfOrderSeat) =
+        seats
+        |> Seq.mapi (fun idx (_, _, seatId) -> idx, seatId)
+        |> Seq.find (fun (idx, seatId) -> (seatId - offset) <> idx)
+
+    outOfOrderSeat - 1

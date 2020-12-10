71636f31d52ff59c2da2d38e7ce40f218951a212
David Sosby <dsosby@gmail.com>
2020-12-09 17:57:36 -0800

    Refactor parser to keep the count

--- Solutions/Day7.fs
+++ Solutions/Day7.fs
@@ -5,6 +5,11 @@ open FParsec
 type Bag = | Bag of string
 let bagify (adjective, color) = Bag (adjective + " " + color)
 
+type Numerous<'a> = | Numerous of int * 'a
+let numerousThing numerous = // TODO Is there a better way than match?
+    match numerous with
+    | Numerous (count, thing) -> thing
+
 let equalBag lhv rhv =
     match lhv, rhv with
     | Bag x, Bag y when x = y -> true
@@ -21,10 +26,10 @@ let space                   = pchar ' '
 let word                    = manyChars letter
 let maybePluralBag          = pstring " bag" .>> many (pchar 's')
 let bag                     = word .>> space .>>. word .>> maybePluralBag |>> bagify
-let counted                 = many1Chars digit >>. space
-let ignoredCountBag         = counted >>. bag
+let counted                 = pint32 .>> space
+let countedBag              = counted .>>. bag |>> Numerous
 let noOtherBags             = stringReturn "no other bags" []
-let listOfBags              = sepBy ignoredCountBag (pstring ", ")
+let listOfBags              = sepBy countedBag (pstring ", ")
 let bagRule                 = bag .>> pstring " contain " .>>. (noOtherBags <|> listOfBags) // Order is important here, as bags may consume "no other
 
 let parseRules parser (input: string) =
@@ -45,10 +50,18 @@ let getHodlers bag rules =
         bagHolders @ descentBagHolders // Not TCO ... hopefully that doesn't kill me
     hodlersOf bag |> Set.ofList
 
+let newRuleToOldRule (rule: (Bag * Numerous<Bag> list)) : (Bag * Bag list) =
+    fst rule, snd rule |> List.map numerousThing
+
 [<Solution(part = 1)>]
 let getCountOfHodlerBags (input: string) =
+    // Originally, part 1 ignored count... now refactoring
     input
     |> parseRules bagRule
+    |> Seq.map newRuleToOldRule
     |> getHodlers shinyGoldBag
     |> Set.count
 
+[<Solution(part = 2)>]
+let getTotalContainedBags (input: string) =
+    "Test"
